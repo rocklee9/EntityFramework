40fab08a17d51d0692394c95e7f5e21f6dd3dbef
Minh Hoang <minhhoang97hk@gmail.com>
2020-04-16 22:34:00 +0700

    Create EDM

---
+++ EntityFramework/EntityFramework/Model/DataBase.Context.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace EntityFramework.Model
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    
+    public partial class DataBase : DbContext
+    {
+        public DataBase()
+            : base("name=DataBase")
+        {
+        }
+    
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+    
+        public virtual DbSet<HocPhan> HocPhans { get; set; }
+        public virtual DbSet<LopHocPhan> LopHocPhans { get; set; }
+        public virtual DbSet<SinhVien> SinhViens { get; set; }
+    }
+}
--- EntityFramework/EntityFramework/Program.cs
+++ EntityFramework/EntityFramework/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace EntityFramework
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
