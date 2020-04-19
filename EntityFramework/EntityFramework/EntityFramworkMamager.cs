9c8803dec672c1fa6ec3614031b36602f8b58ece
Minh Hoang <minhhoang97hk@gmail.com>
2020-04-19 15:43:29 +0700

    tương tác cơ bản với cơ sở dữ liệu

---
+++ EntityFramework/EntityFramework/EntityFramworkMamager.cs
@@ -0,0 +1,61 @@
+﻿using EntityFramework.Model;
+using EntityFramework.Model.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    public class EntityFramworkMamager
+    {
+        DataContext context;
+        public EntityFramworkMamager()
+        {
+            context = new DataContext();
+        }
+
+        public List<SinhVien> GetSinhViens()
+        {
+            return context.SinhViens.ToList();
+        }
+
+        public SinhVien GetSinhVienByMssv(string mssv)
+        {
+            return context.SinhViens.FirstOrDefault(x=>x.MSSV==mssv);
+        }
+
+        public void AddSinhVien(SinhVien sinhVien)
+        {
+            context.SinhViens.Add(new SinhVien
+            {
+                MSSV = sinhVien.MSSV,
+                Age = sinhVien.Age,
+                Birthday = sinhVien.Birthday,
+                Gender = sinhVien.Gender,
+                Name = sinhVien.Name
+            });
+            context.SaveChanges();
+        }
+
+        public void UpdateSinhVien(SinhVien sinhVien)
+        {
+            var sv = context.SinhViens.FirstOrDefault(x => x.MSSV == sinhVien.MSSV);
+
+            sv.Age = sinhVien.Age;
+            sv.Birthday = sinhVien.Birthday;
+            sv.Gender = sinhVien.Gender;
+            sv.Name = sinhVien.Name;
+            
+            context.SaveChanges();
+        }
+
+        public void DeleteSinhVien(string mssv)
+        {
+            var sv = context.SinhViens.FirstOrDefault(x => x.MSSV == mssv);
+            context.SinhViens.Remove(sv);
+            context.SaveChanges();
+        }
+    }
+}
--- EntityFramework/EntityFramework/Program.cs
+++ EntityFramework/EntityFramework/Program.cs
@@ -1,4 +1,5 @@
 ﻿using EntityFramework.Model;
+using EntityFramework.Model.Schema;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,52 @@ namespace EntityFramework
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            DataContext dataContext = new DataContext();
-            dataContext.Database.CreateIfNotExists();
-
-            Console.ReadKey();
+            EntityFramworkMamager ql = new EntityFramworkMamager();
+            string kt;
+            SinhVien sv=new SinhVien();
+            Console.WriteLine("--------------------------------Entity Framework----------------------------");
+            Console.WriteLine("               *********************************************                ");
+            Console.WriteLine("1. Get list sinh viên ");
+            Console.WriteLine("2. Thêm sinh viên");
+            Console.WriteLine("3. Update");
+            Console.WriteLine("4. Delete");
+            Console.WriteLine("5. Exit");
+            Console.WriteLine("---------------------------------------------------------------------------");
+            while (true)
+            {
+                Console.WriteLine("Chon : ");
+                kt = Console.ReadLine();
+                switch (kt)
+                {
+                    case "1":
+                        {
+                            List<SinhVien> sinhViens = ql.GetSinhViens();
+                            Console.WriteLine("Tong so sinh viên {0}",sinhViens.Count);
+                            foreach(var sinhVien in sinhViens)
+                            {
+                                Console.WriteLine("{0}", sinhVien.Name);
+                            }
+                            break;
+                        }
+                    case "2":
+                        {
+                            Console.WriteLine("MSSV =");
+                            sv.MSSV = Console.ReadLine();
+                            Console.WriteLine("Name =");
+                            sv.Name = Console.ReadLine();
+                            Console.WriteLine("Age =");
+                            sv.Age = Convert.ToInt32(Console.ReadLine());
+                            sv.Gender = true;
+                            ql.AddSinhVien(sv);
+                            break;
+                        }
+                }
+                Console.ReadKey();
+            }
+             
         }
     }
 }
